12a18f9b1c7a19983f7b57c1114a0f955b19b39e
KangHyojin1401 <khj6354@naver.com>
2021-05-16 02:13:43 +0900

    create project.
    프로젝트 생성

---
+++ src/main/java/passionx3/jkdk/JkdkApplication.java
@@ -0,0 +1,13 @@
+package passionx3.jkdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkdkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/passionx3/jkdk/JkdkApplicationTests.java
@@ -0,0 +1,13 @@
+package passionx3.jkdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
