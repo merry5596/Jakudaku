15c8192ee1e14e899020aa4f9108cb65265d1d16
jakim09 <jakim09@naver.com>
2021-05-21 16:53:44 +0900

    [Update] LineItemDao, LineItemMapper, MybatisLineItemDao, MybatisOrderDao, OrderDao, OrderMapper

--- src/main/java/passionx3/jkdk/dao/mybatis/mapper/OrderMapper.java
+++ src/main/java/passionx3/jkdk/dao/mybatis/mapper/OrderMapper.java
@@ -1,9 +1,11 @@
 package passionx3.jkdk.dao.mybatis.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Select;
 
+import passionx3.jkdk.domain.LineItem;
 import passionx3.jkdk.domain.Order;
 
 public interface OrderMapper {
@@ -15,4 +17,5 @@ public interface OrderMapper {
 	@Select("SELECT ORDERID, ORDERDATE, TOTALPRICE, CREDITCARD, EXPIREDATE, CARDTYPE, USEDMILEAGE, DISCOUNTCOST, USERID"
 			+ " FROM ORDERS WHERE USERID = #{userId} ORDER BY ORDERDATE")
 	List<Order> getOrdersByUserId(String userId);
+	Map<String, List<Order>> getLineItemsByUserId(String userId);
 }
