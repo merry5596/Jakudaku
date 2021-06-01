d16845f60d57be0c499515e29d8df3b684ac6084
jakim09 <jakim09@naver.com>
2021-06-02 03:00:51 +0900

    [Update] 리뷰 삭제, 회원 탈퇴 알림 구현

--- src/main/java/passionx3/jkdk/controller/ReviewDeleteController.java
+++ src/main/java/passionx3/jkdk/controller/ReviewDeleteController.java
@@ -2,11 +2,13 @@ package passionx3.jkdk.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import passionx3.jkdk.service.jkdkFacade;
+import passionx3.jkdk.util.Message;
 
 @Controller
 @SessionAttributes("userSession")
@@ -20,8 +22,9 @@ public class ReviewDeleteController {
 	}
 	
 	@RequestMapping("/review/removeReview.do")
-	public String handleRequest(@RequestParam("reviewId") int reviewId) throws Exception {
+	public String handleRequest(@RequestParam("reviewId") int reviewId, Model model) throws Exception {
 		jkdkStore.deleteReview(reviewId);
-		return "redirect:/user/myPage/buy.do";
+		model.addAttribute("data", new Message("리뷰가 삭제되었습니다..", "/user/myPage/buy.do"));
+		return "thyme/utils/MessagePage";
 	}
 }
