17f28a9d303031872b771aacaa015082480253b0
miinsun <kor3334@naver.com>
2021-06-21 22:00:45 +0900

    [Update]war 형식 적용

--- src/main/java/passionx3/jkdk/JkdkApplication.java
+++ src/main/java/passionx3/jkdk/JkdkApplication.java
@@ -2,10 +2,17 @@ package passionx3.jkdk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JkdkApplication {
+public class JkdkApplication extends SpringBootServletInitializer{
 
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(JkdkApplication.class);
+	  }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JkdkApplication.class, args);
 	}
